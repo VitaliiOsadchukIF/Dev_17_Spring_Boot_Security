bfcbaa56300aa9a6e79cf77a1c6183f209785bf0
VitaliiOsadchukIF <radistje@gmail.com>
2023-11-30 23:34:08 +0200

    Create Dev_17

--- src/main/java/com/example/Spring_Boot_MVC/entities/Note.java
+++ src/main/java/com/example/Spring_Boot_MVC/entities/Note.java
@@ -1,10 +1,13 @@
 package com.example.Spring_Boot_MVC.entities;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity(name = "note")
 public class Note {
-
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
--- src/main/java/com/example/Spring_Boot_MVC/services/NoteService.java
+++ src/main/java/com/example/Spring_Boot_MVC/services/NoteService.java
@@ -3,72 +3,47 @@ package com.example.Spring_Boot_MVC.services;
 
 import com.example.Spring_Boot_MVC.entities.Note;
 import com.example.Spring_Boot_MVC.exceptions.NotesExceptions;
+import com.example.Spring_Boot_MVC.repository.NoteRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class NoteService implements INoteService {
 
-    private final List<Note> noteList = new ArrayList<>();
-
+    private final NoteRepository repository;
 
     @Override
     public List<Note> listAll() {
-
-        return noteList;
-    }
-
-    private long generateId() {
-
-        if (noteList.isEmpty()) {
-            return 0L;
-        } else {
-            return noteList.size() + 1;
-        }
+        return repository.findAll();
     }
 
     @Override
     public Note add(Note note) {
-        Long id = generateId();
-        note.setId(id);
-        noteList.add(note);
-        return note;
+        return repository.save(note);
     }
 
     @Override
     public void deleteById(Long id) throws NotesExceptions {
-        Note note = getById(id);
-
-        if (note == null) {
-            throw new NotesExceptions(id);
-        } else {
-            noteList.remove(note);
-        }
+        repository.deleteById(id);
     }
 
     @Override
     public Note getById(Long id) throws NotesExceptions {
-        for (Note note : noteList) {
-            if (note.getId() == id) {
-                return note;
-            }
-        }
-        throw new NotesExceptions(id);
+
+        return repository.findById(id).orElseThrow(IllegalAccessError::new);
     }
 
     @Override
     public void update(Note note) throws NotesExceptions {
-        Note existingNote = getById(note.getId());
+       if (repository.existsById(note.getId())) {
+           repository.save(note);
+       }else {
+           throw new NotesExceptions(note.getId());
+       }
 
-        if (existingNote == null) {
-            throw new NotesExceptions(note.getId());
-        } else {
-
-            existingNote.setTitle(note.getTitle());
-            existingNote.setContent(note.getContent());
-        }
     }
 
 
